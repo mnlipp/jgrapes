7be25b8bf2afe53e1010ae93825960820bdb62b7
mnl <mnl@mnl.de>
2013-03-07 21:36:35 +0100

    Removed unused imports.

--- src/org/jdrupes/internal/ComponentCommon.java
+++ src/org/jdrupes/internal/ComponentCommon.java
@@ -20,11 +20,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import java.util.Set;
 
 import org.jdrupes.Channel;
 import org.jdrupes.events.Start;
