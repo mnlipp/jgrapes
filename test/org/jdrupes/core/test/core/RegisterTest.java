fdf481c6db3ce414fd991f69f4d4cee488aad38c
Michael Lipp <mnl@mnl.de>
2014-03-28 09:34:02 +0100

    Added some missing files.

---
+++ test/org/jdrupes/core/test/core/RegisterTest.java
@@ -0,0 +1,52 @@
+package org.jdrupes.core.test.core;
+
+import org.jdrupes.core.AbstractComponent;
+import org.jdrupes.core.Component;
+import org.jdrupes.core.Utils;
+import org.jdrupes.core.annotation.Handler;
+import org.jdrupes.core.events.Attached;
+import org.jdrupes.core.events.Detached;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegisterTest {
+
+	public static class RegisterCatcher extends AbstractComponent {
+		
+		public Component attachParent = null;
+		public Component attachChild = null;
+		public Component detachParent = null;
+		public Component detachChild = null;
+		
+		@Handler(events=Attached.class)
+		public void onAttached(Attached evt) {
+			attachParent = evt.getParent();
+			attachChild = evt.getChild();
+		}
+		
+		@Handler(events=Detached.class)
+		public void onDetached(Detached evt) {
+			detachParent = evt.getParent();
+			detachChild = evt.getChild();
+		}
+	}
+	
+	@Test
+	public void testPostStart() {
+		RegisterCatcher c1 = new RegisterCatcher();
+		RegisterCatcher c2 = new RegisterCatcher();
+		c1.attach(c2);
+		Utils.start(c1);
+		assertEquals(c1, c1.attachParent);
+		assertEquals(c2, c1.attachChild);
+		assertEquals(c1, c2.attachParent);
+		assertEquals(c2, c2.attachChild);
+		c2.detach();
+		assertEquals(c1, c1.detachParent);
+		assertEquals(c2, c1.detachChild);
+		assertEquals(c1, c2.detachParent);
+		assertEquals(c2, c2.detachChild);
+	}
+
+}
