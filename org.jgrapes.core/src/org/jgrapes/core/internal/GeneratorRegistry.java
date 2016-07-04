57ea123758b45014429748dc8054f934073fc063
Michael N. Lipp <mnl@mnl.de>
2016-07-04 20:57:36 +0200

    Added logging support.

--- org.jgrapes.core/src/org/jgrapes/core/internal/GeneratorRegistry.java
+++ org.jgrapes.core/src/org/jgrapes/core/internal/GeneratorRegistry.java
@@ -17,11 +17,22 @@
  */
 package org.jgrapes.core.internal;
 
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jgrapes.core.Component;
+
 /**
  * @author Michael N. Lipp
  *
  */
 public class GeneratorRegistry {
+
+	private static final Logger generatorTracking 
+		= Logger.getLogger(Component.class.getPackage().getName() 
+		+ ".generatorTracking");
 
 	private static final class InstanceHolder {
 		static final GeneratorRegistry INSTANCE = new GeneratorRegistry();
@@ -29,8 +40,12 @@ public class GeneratorRegistry {
 	
 	private long running = 0;
 	private Thread keepAlive;
+	private Map<Object,Object> generators = null;
 	
 	private GeneratorRegistry() {
+		if (generatorTracking.isLoggable(Level.FINE)) {
+			generators = new IdentityHashMap<>();
+		}
 	}
 
 	public static GeneratorRegistry getInstance() {
@@ -39,6 +54,9 @@ public class GeneratorRegistry {
 	
 	synchronized public void add(Object obj) {
 		running += 1;
+		if (generators != null) {
+			generators.put(obj, null);
+		}
 		if (running == 1) {
 			keepAlive = new Thread("GeneratorRegistry") {
 				@Override
@@ -57,6 +75,9 @@ public class GeneratorRegistry {
 	
 	synchronized public void remove(Object obj) {
 		running -= 1;
+		if (generators != null) {
+			generators.remove(obj);
+		}
 		if (running == 0) {
 			keepAlive.interrupt();
 			notifyAll();
@@ -68,17 +89,41 @@ public class GeneratorRegistry {
 	}
 	
 	synchronized public void awaitExhaustion() throws InterruptedException {
+		if (generators != null) {
+			if (running != generators.size()) {
+				generatorTracking.severe
+					("Generator count doesn't match tracked.");
+			}
+		}
 		while (running > 0) {
+			if (generators != null) {
+				generatorTracking.fine
+					("Waiting, generators: " + generators.keySet());
+			}
 			wait();
 		}
 	}
 	
 	synchronized public boolean awaitExhaustion(long timeout) 
 			throws InterruptedException {
+		if (generators != null) {
+			if (running != generators.size()) {
+				generatorTracking.severe
+					("Generator count doesn't match tracked.");
+			}
+		}
 		if (isExhausted()) {
 			return true;
 		}
+		if (generators != null) {
+			generatorTracking.fine
+				("Waiting, generators: " + generators.keySet());
+		}
 		wait(timeout);
+		if (generators != null) {
+			generatorTracking.fine
+				("Waited, generators: " + generators.keySet());
+		}
 		return isExhausted();
 	}
 }
