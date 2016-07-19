ab74333d3d16ab1bcad1a70765fa100140a70f72
Michael N. Lipp <mnl@mnl.de>
2016-07-19 12:19:06 +0200

    Simplified.

--- org.jdrupes.httpcodec/test/org/jdrupes/httpcodec/test/ProtocolErrorsTests.java
+++ org.jdrupes.httpcodec/test/org/jdrupes/httpcodec/test/ProtocolErrorsTests.java
@@ -2,6 +2,7 @@ package org.jdrupes.httpcodec.test;
 
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 
 import org.jdrupes.httpcodec.DecoderResult;
@@ -11,14 +12,12 @@ import org.junit.Test;
 public class ProtocolErrorsTests {
 
 	@Test
-	public void testVersion() {
+	public void testVersion() throws UnsupportedEncodingException {
 		String reqText 
 			= "GET / HTTP/0.8\r\n"
 			+ "Host: localhost:8888\r\n"
 			+ "\r\n";
-		ByteBuffer buffer = ByteBuffer.allocate(8192);
-		buffer.put(reqText.getBytes());
-		buffer.flip();
+		ByteBuffer buffer = ByteBuffer.wrap(reqText.getBytes("ascii"));
 		HttpRequestDecoder decoder = new HttpRequestDecoder();
 		DecoderResult result = decoder.decode(buffer);
 		assertFalse(result.hasRequest());
