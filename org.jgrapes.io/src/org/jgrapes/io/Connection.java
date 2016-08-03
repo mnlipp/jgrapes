a37ab1715fb3a36eb046c9c4f950f68870f1ba93
Michael N. Lipp <mnl@mnl.de>
2016-08-03 16:52:28 +0200

    Improved comments.

--- org.jgrapes.io/src/org/jgrapes/io/Connection.java
+++ org.jgrapes.io/src/org/jgrapes/io/Connection.java
@@ -22,52 +22,51 @@ import org.jgrapes.core.Event;
 import org.jgrapes.core.EventPipeline;
 
 /**
- * Represents an I/O connection. I/O connections are bidirectional but
- * asymmetrical. A connection has an initiator that creates and manages
- * the connection and responders that react to the creation of the
- * connection (and other state changes).
+ * Represents an I/O connection. I/O connections are asymmetrical. A connection
+ * has an initiator that creates and manages the connection and responders that
+ * react to the creation of the connection (and other state changes) and receive
+ * and send data on the connection. Responders can also send an event to close
+ * the connection.
  * <P>
- * Events related to a connection are usually handled by two different
- * pipelines. One pipeline, accessible only to the initiator, handles
- * the events from initiator component to responder components. The other, 
- * made available as a property of the connection, handles the events 
- * from responder to initiator. Of course, any pipeline could be used to 
- * send events to the initiator component. However, using arbitrary 
- * pipelines holds the risk that events aren't delivered in the intended 
- * order.
+ * Events related to a connection are usually handled by two different pipelines
+ * managed by the initiator. One pipeline, accessible only to the initiator,
+ * handles the events from the initiator component to responder components. The
+ * other, made available as a property of the connection, handles the events
+ * from responder to initiator. Of course, any pipeline could be used to send
+ * events to the initiator component. However, using arbitrary pipelines holds
+ * the risk that events aren't delivered in the intended order.
  * 
  * @author Michael N. Lipp
  */
 public interface Connection {
 
 	/**
-	 * Returns A channel that can be used to fire connection related 
-	 * events. This is usually the initiator component's channel. 
-	 * Providing this channel as part of the connection simplifies 
-	 * the implementation of responder components as they don't 
-	 * need any other means to find out where to send response events 
-	 * to. Of course, if the initiator component has a well known channel,
-	 * this well known channel may be used directly as well.
+	 * Returns A channel that can be used to fire connection related events.
+	 * This is usually the initiator component's channel. Providing this channel
+	 * as part of the connection simplifies the implementation of responder
+	 * components as they don't need any other means to find out where to send
+	 * response events to. Of course, if the initiator component has a well
+	 * known channel, this well known channel can be used directly.
 	 * 
 	 * @return the channel
 	 */
 	Channel getChannel();
 
 	/**
-	 * Gets an {@link EventPipeline} that can be used for events going 
-	 * back to the initiator on this connection. Consistently using 
-	 * this event pipeline for response events ensures that the events 
-	 * are written in proper sequence.
+	 * Gets the {@link EventPipeline} that can be used for events going back to
+	 * the initiator of this connection. Consistently using this event pipeline
+	 * for response events ensures that the events are written in proper
+	 * sequence.
 	 */
 	EventPipeline getResponsePipeline();
-	
+
 	/**
-	 * Fires the given event on this connection's channel using 
-	 * the channel's response pipeline. Effectively, 
-	 * {@code respond(someEvent)} is a shortcut for 
-	 * {@code getResponsePipeline.add(someEvent, getChannel())}.
+	 * Fires the given event on this connection's channel using the channel's
+	 * response pipeline. Effectively, {@code respond(someEvent)} is a shortcut
+	 * for {@code getResponsePipeline.add(someEvent, getChannel())}.
 	 * 
-	 * @param event the event to fire
+	 * @param event
+	 *            the event to fire
 	 * @return the event (for easy chaining)
 	 */
 	default <T extends Event<?>> T respond(T event) {
