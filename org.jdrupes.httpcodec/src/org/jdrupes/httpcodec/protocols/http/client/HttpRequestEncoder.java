3f22c5e9ebc6829760f7d3645e04670fb8d482cb
Michael N. Lipp <mnl@mnl.de>
2016-12-08 12:45:05 +0100

    Changed license.

--- org.jdrupes.httpcodec/src/org/jdrupes/httpcodec/protocols/http/client/HttpRequestEncoder.java
+++ org.jdrupes.httpcodec/src/org/jdrupes/httpcodec/protocols/http/client/HttpRequestEncoder.java
@@ -1,20 +1,20 @@
-/*
- * JGrapes Event Driven Framework
+/*******************************************************************************
+ * This file is part of the JDrupes non-blocking HTTP Codec
  * Copyright (C) 2016  Michael N. Lipp
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by 
- * the Free Software Foundation; either version 3 of the License, or 
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or 
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but 
  * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
- * for more details.
- * 
- * You should have received a copy of the GNU General Public License along 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along 
  * with this program; if not, see <http://www.gnu.org/licenses/>.
- */
+ *******************************************************************************/
 package org.jdrupes.httpcodec.protocols.http.client;
 
 import java.io.IOException;
