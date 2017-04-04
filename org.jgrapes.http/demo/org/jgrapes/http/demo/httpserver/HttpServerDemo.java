5f897f97691da17e3a558685cae2027ceef453f0
Michael N. Lipp <mnl@mnl.de>
2017-04-04 10:53:21 +0200

    Added SSL server.

--- org.jgrapes.http/demo/org/jgrapes/http/demo/httpserver/HttpServerDemo.java
+++ org.jgrapes.http/demo/org/jgrapes/http/demo/httpserver/HttpServerDemo.java
@@ -18,18 +18,33 @@
 
 package org.jgrapes.http.demo.httpserver;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.file.Paths;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+
+import org.jgrapes.core.Channel;
 import org.jgrapes.core.Component;
 import org.jgrapes.core.Components;
+import org.jgrapes.core.NamedChannel;
 import org.jgrapes.http.HttpServer;
 import org.jgrapes.http.StaticContentDispatcher;
 import org.jgrapes.http.events.GetRequest;
 import org.jgrapes.http.events.PostRequest;
 import org.jgrapes.io.FileStorage;
 import org.jgrapes.io.NioDispatcher;
+import org.jgrapes.net.SslServer;
+import org.jgrapes.net.TcpServer;
 
 /**
  * @author Michael N. Lipp
@@ -41,14 +56,49 @@ public class HttpServerDemo extends Component {
 	 * @param args
 	 * @throws IOException 
 	 * @throws InterruptedException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws KeyStoreException 
+	 * @throws UnrecoverableKeyException 
+	 * @throws CertificateException 
+	 * @throws KeyManagementException 
 	 */
 	public static void main(String[] args) 
-			throws IOException, InterruptedException {
+			throws IOException, InterruptedException, 
+			NoSuchAlgorithmException, KeyStoreException, 
+			UnrecoverableKeyException, CertificateException, 
+			KeyManagementException {
+		// The demo component is the application
 		HttpServerDemo app = new HttpServerDemo();
+		// Attach a general nio dispatcher
 		app.attach(new NioDispatcher());
+
+		// Network level unencrypted channel.
+		Channel httpTransport = new NamedChannel("httpTransport");
+		// Create a TCP server listening on port 8888
+		app.attach(new TcpServer(httpTransport, new InetSocketAddress(8888)));
+
+		// Create TLS "converter"
+		KeyStore serverStore = KeyStore.getInstance("JKS");
+		try (FileInputStream kf 
+				= new FileInputStream("demo-resources/localhost.jks")) {
+			serverStore.load(kf, "nopass".toCharArray());
+		}
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(
+				KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(serverStore, "nopass".toCharArray());
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+		sslContext.init(kmf.getKeyManagers(), null, new SecureRandom());
+		// Create a TCP server for SSL
+		Channel securedNetwork = app.attach(
+				new TcpServer(SELF, new InetSocketAddress(4443)));
+		app.attach(new SslServer(httpTransport, securedNetwork, sslContext));
+
+		// Create an HTTP server as converter between transport and application
+		// layer.
 		app.attach(new HttpServer(app.channel(), 
-		        new InetSocketAddress(8888), GetRequest.class,
-		        PostRequest.class));
+		        httpTransport, GetRequest.class, PostRequest.class));
+		
+		// Build application layer
 		app.attach(new FileStorage(app.channel(), 65536));
 		app.attach(new StaticContentDispatcher(app.channel(),
 		        "/**", Paths.get("demo-resources/static-content")));
---
+++ org.jgrapes.io/src/org/jgrapes/net/SslServer.java
@@ -0,0 +1,298 @@
+/*
+ * JGrapes Event Driven Framework
+ * Copyright (C) 2017  Michael N. Lipp
+ * 
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jgrapes.net;
+
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLEngineResult;
+import javax.net.ssl.SSLEngineResult.HandshakeStatus;
+import javax.net.ssl.SSLException;
+
+import org.jgrapes.core.Channel;
+import org.jgrapes.core.Component;
+import org.jgrapes.core.annotation.Handler;
+import org.jgrapes.io.IOSubchannel;
+import org.jgrapes.io.events.Close;
+import org.jgrapes.io.events.Closed;
+import org.jgrapes.io.events.Input;
+import org.jgrapes.io.events.Output;
+import org.jgrapes.io.util.LinkedIOSubchannel;
+import org.jgrapes.io.util.ManagedBuffer;
+import org.jgrapes.io.util.ManagedBufferQueue;
+import org.jgrapes.io.util.ManagedByteBuffer;
+import org.jgrapes.net.events.Accepted;
+
+/**
+ * A component that receives and send byte buffers on an
+ * encrypted channel and sends and receives the corresponding
+ * decrypted data on its own channel.
+ */
+public class SslServer extends Component {
+
+	private SSLContext sslContext;
+	
+	/**
+	 * @param componentChannel the component's channel
+	 * @param encryptedChannel the channel with the encrypted data
+	 * @param sslContext the SSL context to use
+	 */
+	public SslServer(Channel componentChannel, Channel encryptedChannel,
+			SSLContext sslContext) {
+		super(componentChannel);
+		this.sslContext = sslContext;
+		Handler.Evaluator.add(
+				this, "onAccepted", encryptedChannel.defaultCriterion());
+		Handler.Evaluator.add(
+				this, "onInput", encryptedChannel.defaultCriterion());
+		Handler.Evaluator.add(
+				this, "onClosed", encryptedChannel.defaultCriterion());
+	}
+
+	/**
+	 * Creates a new downstream connection as {@link LinkedIOSubchannel} 
+	 * of the network connection, a {@link HttpRequestDecoder} and a
+	 * {@link HttpResponseEncoder}.
+	 * 
+	 * @param event
+	 *            the accepted event
+	 */
+	@Handler(dynamic=true)
+	public void onAccepted(Accepted event) {
+		event.forChannels(IOSubchannel.class, DownChannel::new);
+	}
+
+	/**
+	 * Handles encrypted data from upstream (the network). The data is 
+	 * send through the {@link SSLEngine} and events are sent downstream 
+	 * (and in the initial phases upstream) according to the conversion 
+	 * results.
+	 * 
+	 * @param event the event
+	 * @throws InterruptedException 
+	 * @throws SSLException 
+	 */
+	@Handler(dynamic=true)
+	public void onInput(Input<ManagedByteBuffer> event) 
+			throws InterruptedException, SSLException {
+		for (IOSubchannel channel: event.channels(IOSubchannel.class)) {
+			final DownChannel downChannel = (DownChannel) LinkedIOSubchannel
+			        .lookupLinked(channel);
+			if (downChannel == null || downChannel.converterComponent() != this) {
+				continue;
+			}
+			downChannel.sendDownstream(event);
+		}
+	}
+
+	/**
+	 * Handles a close event from the encrypted channel (client).
+	 * 
+	 * @param event the event
+	 * @throws InterruptedException 
+	 * @throws SSLException 
+	 */
+	@Handler(dynamic=true)
+	public void onClosed(Closed event) 
+			throws SSLException, InterruptedException {
+		for (IOSubchannel channel: event.channels(IOSubchannel.class)) {
+			onClosedForChannel(event, channel);
+		}
+	}
+	
+	private void onClosedForChannel(Closed event,
+	        IOSubchannel netChannel) throws SSLException, InterruptedException {
+		final DownChannel downChannel = (DownChannel) LinkedIOSubchannel
+		        .lookupLinked(netChannel);
+		if (downChannel == null || downChannel.converterComponent() != this) {
+			return;
+		}
+		Closed evt = new Closed();
+		downChannel.fire(evt);
+		evt.get();
+		downChannel.sslEngine.closeInbound();
+		while (!downChannel.sslEngine.isOutboundDone()) {
+			ManagedByteBuffer feedback = downChannel.upstreamBuffer();
+			downChannel.sslEngine.wrap(ManagedByteBuffer.EMPTY_BUFFER
+					.backingBuffer(),feedback.backingBuffer());
+			downChannel.upstreamChannel().fire(new Output<>(feedback, false));
+		}
+	}
+
+	
+	/**
+	 * Send decrypted data through the engine and then upstream.
+	 * 
+	 * @param event
+	 *            the event with the data
+	 * @throws InterruptedException if the execution was interrupted
+	 * @throws SSLException if some SSL related problem occurs
+	 */
+	@Handler
+	public void onOutput(Output<ManagedBuffer<ByteBuffer>> event)
+	        throws InterruptedException, SSLException {
+		for (DownChannel downChannel: event.channels(DownChannel.class)) {
+			if (downChannel.converterComponent() != this) {
+				continue;
+			}
+			ByteBuffer output = event.buffer().backingBuffer().duplicate();
+			while (output.hasRemaining()) {
+				ManagedByteBuffer out = downChannel.upstreamBuffer();
+				downChannel.sslEngine.wrap(output,	out.backingBuffer());
+				downChannel.upstreamChannel().fire(new Output<>(
+						out, event.isEndOfRecord()));
+			}
+		}
+	}
+
+	/**
+	 * Handles a close event from downstream.
+	 * 
+	 * @param event
+	 *            the close event
+	 * @throws SSLException if an SSL related problem occurs
+	 * @throws InterruptedException if the execution was interrupted
+	 */
+	@Handler
+	public void onClose(Close event) 
+			throws InterruptedException, SSLException {
+		for (DownChannel connection: event.channels(DownChannel.class)) {
+			if (connection.converterComponent() != this) {
+				continue;
+			}
+			connection.sslEngine.closeOutbound();
+			while (!connection.sslEngine.isOutboundDone()) {
+				ManagedByteBuffer feedback = connection.upstreamBuffer();
+				connection.sslEngine.wrap(ManagedByteBuffer.EMPTY_BUFFER
+						.backingBuffer(),feedback.backingBuffer());
+				connection.upstreamChannel().fire(new Output<>(feedback, false));
+			}
+			connection.upstreamChannel().fire(new Close());
+		}		
+	}
+	
+	private class DownChannel extends LinkedIOSubchannel {
+		public SocketAddress localAddress;
+		public SocketAddress remoteAddress;
+		public SSLEngine sslEngine;
+		private ManagedBufferQueue<ManagedByteBuffer, ByteBuffer> downstreamPool;
+		private ManagedBufferQueue<ManagedByteBuffer, ByteBuffer> upstreamPool;
+
+		public DownChannel(Accepted event, IOSubchannel upstreamChannel) {
+			super(SslServer.this, upstreamChannel);
+			localAddress = event.localAddress();
+			remoteAddress = event.remoteAddress();
+			if (remoteAddress instanceof InetSocketAddress) {
+				sslEngine = sslContext.createSSLEngine(
+						((InetSocketAddress)remoteAddress).getAddress().getHostAddress(),
+						((InetSocketAddress)remoteAddress).getPort());
+			} else {
+				sslEngine = sslContext.createSSLEngine();
+			}
+			sslEngine.setUseClientMode(false);
+		}
+		
+		@Override
+		public ManagedBufferQueue<ManagedByteBuffer, ByteBuffer> bufferPool() {
+			if (downstreamPool == null) {
+				// Adding 50, see https://docs.oracle.com/javase/8/docs/technotes/guides/security/jsse/samples/sslengine/SSLEngineSimpleDemo.java
+				int bufSize = sslEngine.getSession()
+						.getApplicationBufferSize() + 50;
+				downstreamPool = new ManagedBufferQueue<>(ManagedByteBuffer.class,
+						ByteBuffer.allocate(bufSize), 
+						ByteBuffer.allocate(bufSize));
+			}
+			return downstreamPool;
+		}
+		
+		public ManagedByteBuffer upstreamBuffer() throws InterruptedException {
+			if (upstreamPool == null) {
+				ManagedByteBuffer testBuf = upstreamChannel().bufferPool().acquire();
+				if (testBuf.capacity() 
+						>= sslEngine.getSession().getPacketBufferSize() + 50) {
+					upstreamPool = upstreamChannel().bufferPool();
+					return testBuf;
+				} else {
+					int bufSize = sslEngine.getSession().getPacketBufferSize() + 50;
+					upstreamPool = new ManagedBufferQueue<>(ManagedByteBuffer.class,
+							ByteBuffer.allocate(bufSize), 
+							ByteBuffer.allocate(bufSize));
+				}
+			}
+			return upstreamPool.acquire();
+		}
+		
+		public void sendDownstream(Input<ManagedByteBuffer> event)
+				throws SSLException, InterruptedException {
+			ManagedByteBuffer unwrapped = bufferPool().acquire();
+			ByteBuffer input = event.buffer().duplicate();
+			while (true) {
+				SSLEngineResult unwrapResult = sslEngine.unwrap(
+						input, unwrapped.backingBuffer());
+				switch (unwrapResult.getHandshakeStatus()) {
+				case NEED_TASK:
+					while (true) {
+						Runnable runnable = sslEngine.getDelegatedTask();
+						if (runnable == null) {
+							break;
+						}
+						runnable.run();
+					}
+					continue;
+					
+				case NEED_WRAP:
+					ManagedByteBuffer feedback = upstreamBuffer();
+					SSLEngineResult wrapResult = sslEngine.wrap(
+							ManagedByteBuffer.EMPTY_BUFFER
+							.backingBuffer(), feedback.backingBuffer());
+					upstreamChannel().fire(new Output<>(feedback, false));
+					if (wrapResult.getHandshakeStatus() == HandshakeStatus.FINISHED) {
+						fire(new Accepted(localAddress, remoteAddress));
+					}
+					continue;
+					
+				case FINISHED:
+					fire(new Accepted(localAddress, remoteAddress));
+					break;
+					
+				case NEED_UNWRAP:
+					if (input.hasRemaining()) {
+						continue;
+					}
+					break;
+					
+				default:
+					break;
+				}
+				break;
+			}
+			if (unwrapped.position() == 0) {
+				// Was only handshake
+				unwrapped.unlockBuffer();
+				return;
+			}
+			unwrapped.flip();
+			fire(new Input<>(unwrapped, sslEngine.isInboundDone()));
+		}
+	}
+
+}
