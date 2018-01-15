1e6b458b97c9b5c47332d44788412806a1b23003
Michael N. Lipp <mnl@mnl.de>
2018-01-15 21:33:04 +0100

    Added Hello World example.

---
+++ examples/src/org/jgrapes/examples/core/helloworld/Greeter.java
@@ -0,0 +1,40 @@
+/*
+ * JGrapes Event Driven Framework
+ * Copyright (C) 2017  Michael N. Lipp
+ * 
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along 
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jgrapes.examples.core.helloworld;
+
+import org.jgrapes.core.Component;
+import org.jgrapes.core.Components;
+import org.jgrapes.core.annotation.Handler;
+import org.jgrapes.core.events.Start;
+
+/**
+ * A component that displays "Hello World" on start.
+ */
+public class Greeter extends Component {
+
+	@Handler
+	public void onStart(Start event) {
+		System.out.println ("Hello World!");
+	}
+	
+	public static void main(String[] args) 
+			throws InterruptedException {
+		Components.start(new Greeter());
+	}
+}
