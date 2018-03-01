ebf735a27becb4128ef16b477b2638b5542edea9
Michael N. Lipp <mnl@mnl.de>
2018-03-01 18:51:13 +0100

    Optimized, fixed close event.

---
+++ org.jgrapes.io/test/org/jgrapes/io/test/InputStreamTests.java
@@ -0,0 +1,88 @@
+package org.jgrapes.io.test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+
+import org.jgrapes.core.Channel;
+import org.jgrapes.core.Component;
+import org.jgrapes.core.Components;
+import org.jgrapes.core.annotation.Handler;
+import org.jgrapes.io.IOSubchannel;
+import org.jgrapes.io.events.Close;
+import org.jgrapes.io.events.Output;
+import org.jgrapes.io.util.InputStreamPipeline;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InputStreamTests {
+
+	public static class Tracker extends Component {
+
+		public int outputs = 0;
+		public long collected = 0;
+		public int eors = 0;
+		public int closed = 0;
+		
+		public Tracker() {
+			super(Channel.SELF);
+		}
+		
+		@Handler
+		public void onOutput(Output<ByteBuffer> event, IOSubchannel channel) 
+				throws UnsupportedEncodingException {
+			outputs += 1;
+			int length = event.data().limit();
+			collected += length;
+			if (event.isEndOfRecord()) {
+				eors += 1;
+			}
+		}
+		
+		@Handler
+		public void onClose(Close event, IOSubchannel channel) {
+			closed += 1;
+		}
+	}
+	
+	@Test
+	public void testEorAndClose()
+			throws InterruptedException, IOException {
+		Tracker tracker = new Tracker();
+		Components.start(tracker);
+		IOSubchannel channel = IOSubchannel.create(
+				tracker, tracker.newEventPipeline());
+		ByteArrayInputStream in = new ByteArrayInputStream("Test".getBytes());
+		InputStreamPipeline isp = new InputStreamPipeline(in, channel);
+		isp.run();
+		Components.awaitExhaustion();
+		Components.checkAssertions();
+		assertEquals(1, tracker.outputs);
+		assertTrue(tracker.collected > 0);
+		assertEquals(1, tracker.eors);
+		assertEquals(1, tracker.closed);
+	}
+
+	@Test
+	public void testEorNoClose()
+			throws InterruptedException, IOException {
+		Tracker tracker = new Tracker();
+		Components.start(tracker);
+		IOSubchannel channel = IOSubchannel.create(
+				tracker, tracker.newEventPipeline());
+		ByteArrayInputStream in = new ByteArrayInputStream("Test".getBytes());
+		InputStreamPipeline isp = new InputStreamPipeline(in, channel)
+				.suppressClose();
+		isp.run();
+		Components.awaitExhaustion();
+		Components.checkAssertions();
+		assertEquals(1, tracker.outputs);
+		assertTrue(tracker.collected > 0);
+		assertEquals(1, tracker.eors);
+		assertEquals(0, tracker.closed);
+	}
+
+}
