f3e60a4f63985ce4f52831c0b6af3c0f420ce69b
Michael N. Lipp <mnl@mnl.de>
2018-07-01 17:23:57 +0200

    Jdk 9 SE compatibility.

--- org.jgrapes.core/src/org/jgrapes/core/internal/ComponentProxy.java
+++ org.jgrapes.core/src/org/jgrapes/core/internal/ComponentProxy.java
@@ -118,7 +118,7 @@ public final class ComponentProxy extends ComponentVertex {
      * @param componentChannel the component's channel
      * @return the node representing the component in the tree
      */
-    @SuppressWarnings("PMD.DataflowAnomalyAnalysis")
+    @SuppressWarnings({ "PMD.DataflowAnomalyAnalysis", "deprecation" })
     /* default */ static ComponentVertex getComponentProxy(
             ComponentType component, Channel componentChannel) {
         ComponentProxy componentProxy = null;
