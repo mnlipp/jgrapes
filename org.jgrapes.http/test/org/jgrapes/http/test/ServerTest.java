adc216b85f6407c5e96c2d72a0884a72e8d22a53
Michael N. Lipp <mnl@mnl.de>
2018-09-10 18:59:45 +0200

    Minor enhancement.

--- org.jgrapes.http/test/org/jgrapes/http/test/ServerTest.java
+++ org.jgrapes.http/test/org/jgrapes/http/test/ServerTest.java
@@ -75,207 +75,211 @@ import org.junit.Test;
 
 public class ServerTest {
 
-	private static TestServer server;
+    private static TestServer server;
 
-	int minTime = 0;
-	int maxTime = 0;
-	long accumTime = 0;
-	int calls = 0;
-	
-	public static class TestServer extends Component {
+    int minTime = 0;
+    int maxTime = 0;
+    long accumTime = 0;
+    int calls = 0;
 
-		private InetSocketAddress addr;
-		private WaitForTests readyMonitor;
+    public static class TestServer extends Component {
 
-		public TestServer() throws IOException, KeyStoreException, 
-			NoSuchAlgorithmException, CertificateException, 
-			UnrecoverableKeyException, KeyManagementException {
-			super();
-			// Attach a general nio dispatcher
-			attach(new NioDispatcher());
+        private InetSocketAddress addr;
+        private WaitForTests readyMonitor;
 
-			// Create TLS "converter"
-			KeyStore serverStore = KeyStore.getInstance("JKS");
-			try (FileInputStream kf 
-					= new FileInputStream("test-resources/localhost.jks")) {
-				serverStore.load(kf, "nopass".toCharArray());
-			}
-			KeyManagerFactory kmf = KeyManagerFactory.getInstance(
-					KeyManagerFactory.getDefaultAlgorithm());
-	        kmf.init(serverStore, "nopass".toCharArray());
-			SSLContext sslContext = SSLContext.getInstance("TLS");
-			sslContext.init(kmf.getKeyManagers(), null, new SecureRandom());
-			
-			// Network level unencrypted channel.
-			Channel httpTransport = new NamedChannel("httpTransport");
+        public TestServer() throws IOException, KeyStoreException,
+                NoSuchAlgorithmException, CertificateException,
+                UnrecoverableKeyException, KeyManagementException {
+            super();
+            // Attach a general nio dispatcher
+            attach(new NioDispatcher());
 
-			// Create a TCP server for SSL
-			TcpServer securedNetwork = attach(new TcpServer()
-					.setBacklog(3000)
-					.setConnectionLimiter(new PermitsPool(50)));
-			attach(new SslServer(httpTransport, securedNetwork, sslContext));
+            // Create TLS "converter"
+            KeyStore serverStore = KeyStore.getInstance("JKS");
+            try (FileInputStream kf
+                = new FileInputStream("test-resources/localhost.jks")) {
+                serverStore.load(kf, "nopass".toCharArray());
+            }
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(
+                KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(serverStore, "nopass".toCharArray());
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), null, new SecureRandom());
 
-			// Create an HTTP server as converter between transport and application
-			// layer.
-			attach(new HttpServer(channel(), 
-			        httpTransport, GetRequest.class, PostRequest.class)
-					.setAcceptNoSni(true));
-			
-			// Build application layer
-			attach(new FileStorage(channel(), 65536));
-			attach(new StaticContentDispatcher(channel(),
-					"/**", Paths.get("test-resources/static-content").toUri()));
-			attach(new PurgeTerminator(channel()));
-			
-			readyMonitor = new WaitForTests(this, Ready.class, 
-					securedNetwork.channel().defaultCriterion());
-		}
-		
-		public InetSocketAddress getSocketAddress()
-		        throws InterruptedException, ExecutionException {
-			if (addr == null) {
-				Ready readyEvent = (Ready) readyMonitor.get();
-				if (!(readyEvent
-				        .listenAddress() instanceof InetSocketAddress)) {
-					fail();
-				}
-				addr = ((InetSocketAddress) readyEvent.listenAddress());
-			}
-			return addr;
-		}
-	
-		public int getPort()
-		        throws InterruptedException, ExecutionException {
-			return getSocketAddress().getPort();
-		}
-	}
+            // Network level unencrypted channel.
+            Channel httpTransport = new NamedChannel("httpTransport");
 
-	@BeforeClass
-	public static void startServer() throws IOException, InterruptedException, 
-			ExecutionException, UnrecoverableKeyException, 
-			KeyManagementException, KeyStoreException, NoSuchAlgorithmException, 
-			CertificateException {
-		server = new TestServer();
-		Components.start(server);
+            // Create a TCP server for SSL
+            TcpServer securedNetwork = attach(new TcpServer()
+                .setBacklog(3000)
+                .setConnectionLimiter(new PermitsPool(50)));
+            attach(new SslServer(httpTransport, securedNetwork, sslContext));
 
-		// Create a trust manager that does not validate certificate chains
-		TrustManager[] trustAllCerts = new TrustManager[]{
-		    new X509TrustManager() {
-		        public X509Certificate[] getAcceptedIssuers() {
-		        	return null;
-		        }
-		        
-		        public void checkClientTrusted(
-		            X509Certificate[] certs, String authType) {
-		        }
-		        
-		        public void checkServerTrusted(
-		            X509Certificate[] certs, String authType) {
-		        }
-		    }
-		};
+            // Create an HTTP server as converter between transport and
+            // application
+            // layer.
+            attach(new HttpServer(channel(),
+                httpTransport, GetRequest.class, PostRequest.class)
+                    .setAcceptNoSni(true));
 
-		SSLContext sc = SSLContext.getInstance("SSL");
-	    sc.init(null, trustAllCerts, null);
-		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-		
-		// Create all-trusting host name verifier
+            // Build application layer
+            attach(new FileStorage(channel(), 65536));
+            attach(new StaticContentDispatcher(channel(),
+                "/**", Paths.get("test-resources/static-content").toUri()));
+            attach(new PurgeTerminator(channel()));
+
+            readyMonitor = new WaitForTests(this, Ready.class,
+                securedNetwork.channel().defaultCriterion());
+        }
+
+        public InetSocketAddress getSocketAddress()
+                throws InterruptedException, ExecutionException {
+            if (addr == null) {
+                Ready readyEvent = (Ready) readyMonitor.get();
+                if (!(readyEvent
+                    .listenAddress() instanceof InetSocketAddress)) {
+                    fail();
+                }
+                addr = ((InetSocketAddress) readyEvent.listenAddress());
+            }
+            return addr;
+        }
+
+        public int getPort()
+                throws InterruptedException, ExecutionException {
+            return getSocketAddress().getPort();
+        }
+    }
+
+    @BeforeClass
+    public static void startServer() throws IOException, InterruptedException,
+            ExecutionException, UnrecoverableKeyException,
+            KeyManagementException, KeyStoreException, NoSuchAlgorithmException,
+            CertificateException {
+        server = new TestServer();
+        Components.start(server);
+
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] {
+            new X509TrustManager() {
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(
+                        X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(
+                        X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
         HostnameVerifier allHostsValid = new HostnameVerifier() {
             public boolean verify(String hostname, SSLSession session) {
                 return true;
             }
         };
- 
+
         // Install the all-trusting host verifier
         HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
 
-	}
-	
-	@AfterClass
-	public static void stopServer() throws InterruptedException {
-		server.fire(new Stop(), Channel.BROADCAST);
-		Components.awaitExhaustion();
-		Components.checkAssertions();
-	}
-	
-	@Test
-	public void testConcurrentGetRoot() 
-			throws IOException, InterruptedException, ExecutionException, 
-			TimeoutException {
-		Waiter waiter = new Waiter();
-		
-		URL url = new URL("https", "localhost", server.getPort(), "/");
-		
-		int threadCount = 1000;
-		if (Boolean.parseBoolean(
-				System.getenv().getOrDefault("TRAVIS", "false"))) {
-			threadCount = 100;
-		}
-		
-		final List<Thread> threads = new ArrayList<>();
-		AtomicInteger pending = new AtomicInteger(0);
-		for (int i = 0; i < threadCount; i++) {
-			Thread getThread = new Thread() {
-				@Override
-				public void run() {
-					try {
-						synchronized (threads) {
-							try {
-								pending.incrementAndGet();
-								threads.wait();
-							} catch (InterruptedException e) {
-								// ignored
-							}
-						}
-						long start = System.currentTimeMillis();
-						HttpURLConnection conn = (HttpURLConnection) url
-						        .openConnection();
-						String content;
-			            try (BufferedReader buffer = new BufferedReader(
-			            		new InputStreamReader(conn.getInputStream()))) {
-			                    content = buffer.lines().collect(Collectors.joining("\n"));
-			            }
-						long stop = System.currentTimeMillis();
-						int used = (int)(stop - start);
-						synchronized (ServerTest.this) {
-							if (calls == 0) {
-								minTime = used;
-								maxTime = used;
-							} else {
-								minTime = Math.min(minTime,  used);
-								maxTime = Math.max(maxTime, used);
-							}
-							calls += 1;
-							accumTime += used;
-						}
-						waiter.assertEquals(200, conn.getResponseCode());
-			            waiter.assertTrue(content.contains("Demo"));
-					} catch (IOException e) {
-						waiter.fail(e);
-					} finally {
-						waiter.resume();
-					}
-				}
-			};
-			threads.add(getThread);
-			getThread.start();
-		}
-		while (pending.get() < threads.size()) {
-			Thread.sleep(100);
-		}
-		Thread.sleep(100);
-		synchronized (threads) {
-			threads.notifyAll();
-		}
-		waiter.await(60000, threads.size());
-		
-		// Cleanup.
-		while (threads.size() > 0) {
-			threads.remove(0).join();
-		}
-		
-		System.out.println("min/avg/max (ms): " 
-				+ minTime + "/" + (accumTime / calls) + "/" + maxTime);
-	}
-	
+    }
+
+    @AfterClass
+    public static void stopServer() throws InterruptedException {
+        server.fire(new Stop(), Channel.BROADCAST);
+        Components.awaitExhaustion();
+        Components.checkAssertions();
+    }
+
+    @Test
+    public void testConcurrentGetRoot()
+            throws IOException, InterruptedException, ExecutionException,
+            TimeoutException {
+        Waiter waiter = new Waiter();
+
+        URL url = new URL("https", "localhost", server.getPort(), "/");
+
+        int threadCount = 1000;
+        if (Boolean.parseBoolean(
+            System.getenv().getOrDefault("TRAVIS", "false"))) {
+            threadCount = 100;
+        }
+
+        final List<Thread> threads = new ArrayList<>();
+        AtomicInteger pending = new AtomicInteger(0);
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNumber = i;
+            Thread getThread = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        synchronized (threads) {
+                            try {
+                                pending.incrementAndGet();
+                                threads.wait();
+                            } catch (InterruptedException e) {
+                                // ignored
+                            }
+                        }
+                        setName("Getter " + threadNumber);
+                        long start = System.currentTimeMillis();
+                        HttpURLConnection conn = (HttpURLConnection) url
+                            .openConnection();
+                        String content;
+                        try (BufferedReader buffer = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()))) {
+                            content = buffer.lines()
+                                .collect(Collectors.joining("\n"));
+                        }
+                        long stop = System.currentTimeMillis();
+                        int used = (int) (stop - start);
+                        synchronized (ServerTest.this) {
+                            if (calls == 0) {
+                                minTime = used;
+                                maxTime = used;
+                            } else {
+                                minTime = Math.min(minTime, used);
+                                maxTime = Math.max(maxTime, used);
+                            }
+                            calls += 1;
+                            accumTime += used;
+                        }
+                        waiter.assertEquals(200, conn.getResponseCode());
+                        waiter.assertTrue(content.contains("Demo"));
+                    } catch (IOException e) {
+                        waiter.fail(e);
+                    } finally {
+                        waiter.resume();
+                    }
+                }
+            };
+            threads.add(getThread);
+            getThread.start();
+        }
+        while (pending.get() < threads.size()) {
+            Thread.sleep(100);
+        }
+        Thread.sleep(100);
+        synchronized (threads) {
+            threads.notifyAll();
+        }
+        waiter.await(60000, threads.size());
+
+        // Cleanup.
+        while (threads.size() > 0) {
+            threads.remove(0).join();
+        }
+
+        System.out.println("min/avg/max (ms): "
+            + minTime + "/" + (accumTime / calls) + "/" + maxTime);
+    }
+
 }
