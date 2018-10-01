af11aee88e5a6f849c8ea2587bfa0d10a667b799
Michael N. Lipp <mnl@mnl.de>
2018-10-01 13:45:21 +0200

    Simplified.

--- org.jgrapes.core/src/org/jgrapes/core/TypedIdKey.java
+++ org.jgrapes.core/src/org/jgrapes/core/TypedIdKey.java
@@ -22,14 +22,23 @@ import java.io.Serializable;
 import java.util.Map;
 import java.util.Optional;
 
-import org.jgrapes.core.Associator;
-
 /**
  * Combines a type and an id value to a key for an {@link Associator}
- * or {@link Map}. This kind of key can be used if using an instance of 
- * the type as key is too specific or not desirable due to garbage collection. 
+ * or {@link Map}. This kind of key can be used if an instance of 
+ * the type as key is too specific (or objects should not be kept alive
+ * only because they are used as keys) and the identifier alone cannot 
+ * be guaranteed to be unique.
  */
-public class TypedIdKey<V> {
+@SuppressWarnings("serial")
+public class TypedIdKey<V> implements Serializable {
+
+    private Class<V> type;
+    private Serializable id;
+
+    private TypedIdKey(Class<V> type, Serializable id) {
+        this.type = type;
+        this.id = id;
+    }
 
     /**
      * Associates the given value's type and the id with the given value
@@ -41,9 +50,11 @@ public class TypedIdKey<V> {
      * @param value the value
      * @return the value for easy chaining
      */
-    public static <V> V associate(Associator associator, Object id, V value) {
-        associator.setAssociated(new TypeIdPair<>(value.getClass(), id),
-            value);
+    @SuppressWarnings({ "PMD.ShortVariable", "unchecked" })
+    public static <V> V associate(Associator associator, Serializable id,
+            V value) {
+        associator.setAssociated(
+            new TypedIdKey<V>((Class<V>) value.getClass(), id), value);
         return value;
     }
 
@@ -57,24 +68,10 @@ public class TypedIdKey<V> {
      * @param value the value
      * @return the value for easy chaining
      */
-    public static <V> V put(Map<Object, ? super V> map, Object id, V value) {
-        map.put(new TypeIdPair<>(value.getClass(), id), value);
-        return value;
-    }
-
-    /**
-     * Associates the given value's type and the id with the given value
-     * using the given map. Special version for maps that must be serializable.
-     *
-     * @param <V> the value type
-     * @param map the map
-     * @param id the id
-     * @param value the value
-     * @return the value for easy chaining
-     */
-    public static <V extends Serializable> V
-            put(Map<Serializable, ? super V> map, Serializable id, V value) {
-        map.put(new TypeIdPair<>(value.getClass(), id), value);
+    @SuppressWarnings({ "PMD.ShortVariable", "unchecked" })
+    public static <V> V put(Map<? super TypedIdKey<V>, ? super V> map,
+            Serializable id, V value) {
+        map.put(new TypedIdKey<V>((Class<V>) value.getClass(), id), value);
         return value;
     }
 
@@ -87,100 +84,68 @@ public class TypedIdKey<V> {
      * @param id the id
      * @return the associated value, if any
      */
+    @SuppressWarnings("PMD.ShortVariable")
     public static <V> Optional<V> associated(Associator associator,
-            Class<V> type, Object id) {
-        return associator.associated(new TypeIdPair<>(type, id), type);
-    }
-
-    /**
-     * Retrieves a value with the given type and id from the given map.
-     *
-     * @param <V> the value type
-     * @param map the map
-     * @param type the type
-     * @param id the id
-     * @return the associated value, if any
-     */
-    @SuppressWarnings("unchecked")
-    public static <V> Optional<V> get(Map<Object, Object> map,
-            Class<V> type, Object id) {
-        return Optional
-            .ofNullable((V) map.get(new TypeIdPair<>(type, id)));
-    }
-
-    /**
-     * Retrieves a value with the given type and id from the given map.
-     * special version for maps that must be serializable.
-     *
-     * @param <V> the value type
-     * @param map the map
-     * @param type the type
-     * @param id the id
-     * @return the associated value, if any
-     */
-    @SuppressWarnings("unchecked")
-    public static <V extends Serializable> Optional<V> get(
-            Map<Serializable, Serializable> map,
             Class<V> type, Serializable id) {
-        return Optional
-            .ofNullable((V) map.get(new TypeIdPair<>(type, id)));
+        return associator.associated(new TypedIdKey<>(type, id), type);
     }
 
-    @SuppressWarnings("serial")
-    private static class TypeIdPair<V> implements Serializable {
+    /**
+     * Retrieves a value with the given type and id from the given map.
+     *
+     * @param <V> the value type
+     * @param map the map
+     * @param type the type
+     * @param id the id
+     * @return the associated value, if any
+     */
+    @SuppressWarnings({ "unchecked", "PMD.ShortVariable" })
+    public static <V> Optional<V> get(Map<?, ?> map, Class<V> type,
+            Serializable id) {
+        return Optional
+            .ofNullable((V) map.get(new TypedIdKey<>(type, id)));
+    }
 
-        protected Class<V> type;
-        protected Object id;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
 
-        public TypeIdPair(Class<V> type, Object id) {
-            super();
-            this.type = type;
-            this.id = id;
-        }
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((id == null) ? 0 : id.hashCode());
-            result = prime * result + ((type == null) ? 0 : type.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            TypeIdPair<?> other = (TypeIdPair<?>) obj;
-            if (id == null) {
-                if (other.id != null) {
-                    return false;
-                }
-            } else if (!id.equals(other.id)) {
-                return false;
-            }
-            if (type == null) {
-                if (other.type != null) {
-                    return false;
-                }
-            } else if (!type.equals(other.type)) {
-                return false;
-            }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
             return true;
         }
-
-        @Override
-        public String toString() {
-            return "TypedIdKey [" + type + ":" + id + "]";
+        if (obj == null) {
+            return false;
         }
-
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        TypedIdKey<?> other = (TypedIdKey<?>) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        if (type == null) {
+            if (other.type != null) {
+                return false;
+            }
+        } else if (!type.equals(other.type)) {
+            return false;
+        }
+        return true;
     }
 
+    @Override
+    public String toString() {
+        return "TypedIdKey [" + type + ":" + id + "]";
+    }
 }
